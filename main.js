613fc66dbc21535d800491e16c5651c50d570eb7
Hridaan12 <71926511+Hridaan12@users.noreply.github.com>
2021-04-29 11:49:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function setup(){
+    canvas = createCanvas(600,500)
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+function preload(){
+    song = loadSound("music.mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded(){
+console.log('poseNet is Initialized')
+}
+function gotPoses(results)
+{
+ 
+
+    }
